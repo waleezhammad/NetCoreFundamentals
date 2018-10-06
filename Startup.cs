40758af4d31d5f0fb0d260d28eee40ed657d4ab6
Waleed <eng.walid2007@gmail.com>
2018-10-06 23:06:55 +0300

    Getting Greeeings from Json File

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCoreFundamentals
 {
@@ -18,7 +19,7 @@ namespace NetCoreFundamentals
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configure)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +28,8 @@ namespace NetCoreFundamentals
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = configure["Greetings"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
